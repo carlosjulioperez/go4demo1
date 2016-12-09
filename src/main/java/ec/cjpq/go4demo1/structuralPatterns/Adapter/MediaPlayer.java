22f0b2e48fe0a28a468fa4f1be5df2d0e35ae900
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2016-12-08 19:10:23 -0500

    Adapter Pattern.

---
+++ src/main/java/ec/cjpq/go4demo1/structuralPatterns/Adapter/MediaPlayer.java
@@ -0,0 +1,13 @@
+package ec.cjpq.go4demo1.structuralPatterns.Adapter;
+
+/**
+ * Gang of Four Design Patterns Demo 1
+ * MediaPlayer interface
+ * @author carper, carlosjulioperez@gmail.com
+ * 2016-12-08
+ */
+public interface MediaPlayer{
+
+	public void play(String audioType, String fileName);
+
+}
---
+++ src/main/java/ec/cjpq/go4demo1/structuralPatterns/Adapter/Mp4Player.java
@@ -0,0 +1,21 @@
+package ec.cjpq.go4demo1.structuralPatterns.Adapter;
+
+/**
+ * Gang of Four Design Patterns Demo 1
+ * Mp4Player concrete class implementing AdvancedMediaPlayer interface
+ * @author carper, carlosjulioperez@gmail.com
+ * 2016-12-08
+ */
+public class Mp4Player implements AdvancedMediaPlayer{
+
+	@Override
+	public void playVlc(String fileName){
+		//Do nothing...	
+	}
+	
+	@Override
+	public void playMp4(String fileName){
+		System.out.println("Playing mp4 file. Name: " + fileName);
+	}
+
+}
